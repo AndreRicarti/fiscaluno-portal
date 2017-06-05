f72b95d20b80a76320f34269f86ea682f391d520
Andre Ricarti <a.ricarti@qiwi.com>
2017-06-05 13:36:30 -0300

    publish website

--- assets/pages/cadastro/agente/agente-main.js
+++ assets/pages/cadastro/agente/agente-main.js
@@ -42,7 +42,7 @@ function getDealerData(estruturaID, estadoID, cidadeID, dealerEnabled) {
     $("#div-dealer-table").html("Carregando...");
 
     $.ajax({
-        url: 'cadastro/avaliacoesalunos/get',
+        url: 'cadastro/AvaliacoesAlunos/get',
         type: 'POST',
         dataType: 'json',
         data: {
